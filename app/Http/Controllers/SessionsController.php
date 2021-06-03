af750366f7180b2974906b170342f90b4080e170
silasiqq <41629820@qq.com>
2021-06-03 10:46:36 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,6 +13,7 @@ class SessionsController extends Controller
     {
         return view('sessions.create');
     }
+
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -26,5 +27,12 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，你的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::resource('users', 'UsersController');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
-Route::get('logout', 'SessionsController@destroy')->name('logout');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
